edc7df68a1b3d47a9587230f5f9af299d853d7cc
Shriyansh-SRT <shriyanshraj.timsina@gmail.com>
2025-02-08 12:04:27 +0545

    updates

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import appwriteService from '../appwrite/config'
+import service from '../appwrite/config'
 import {Link} from "react-router-dom"
 
 const PostCard = ({$id, title, featuredImage, content}) => {
   return (
     <Link to={`/post/${$id}`}>
       <div className='w-full bg-gray-400 rounded-xl'>
-        <div className='relative inline-block h-auto w-full overflow-hidden rounded-xl bg-cover bg-center' style={{backgroundImage: `url(${appwriteService.getFilePreview(featuredImage, 300, 200)})`}}>
+        <div className='relative inline-block h-auto w-full overflow-hidden rounded-xl bg-cover bg-center' 
+        style={{backgroundImage: `url(${service.getFilePreview(featuredImage, 300, 200)})`}}>
           <span className='absolute right-2 bottom-2 inline-flex rounded-full bg-black px-2 py-1 text-xs font-semibold uppercase leading-none text-white'>
             {content}
           </span>
